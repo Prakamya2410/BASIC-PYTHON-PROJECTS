62ab388d87c2667a5d35d3d064a0da22e7473705
PRAKAMYA SHRIVAS <126973768+Prakamya2410@users.noreply.github.com>
2023-03-06 18:39:44 +0530

    Add files via upload

---
+++ intrest calculator/simple intrest calculator.py
@@ -0,0 +1,7 @@
+print ("SIMPLE INTREST CALCULATION\n")
+print ("enter the following details.")
+p =int(input ("amount:"))
+n =int(input ("no.of years:"))
+r =int(input ("rate of intrest:"))
+i = (p*n*r) / 100
+print ("\n the intrest is : " + str(i))
