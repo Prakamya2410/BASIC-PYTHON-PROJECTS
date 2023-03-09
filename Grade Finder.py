2b6653672db75e6362f2941b1f24a82108f31279
PRAKAMYA SHRIVAS <126973768+Prakamya2410@users.noreply.github.com>
2023-03-09 11:20:01 +0530

    Grade Finder
    it accepts percentage of the student and display its grade acordingly

---
+++ Grade Finder.py
@@ -0,0 +1,11 @@
+perc = float(input("enter percentage of the student: "))
+if perc > 85:
+    print ('A')
+elif perc >70 and perc <=85:
+    print ('B')
+elif perc > 60 and perc <=70:
+    print ('C')
+elif perc >45 and perc <=60:
+    print ('D')
+else:
+    print ('E')
